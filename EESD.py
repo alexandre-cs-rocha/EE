ec116325a0f7712b8a6d012518e821007df42485
alexandre-cs-rocha <129331327+alexandre-cs-rocha@users.noreply.github.com>
2023-06-20 21:53:42 -0300

    Add files via upload

---
+++ EESD.py
@@ -0,0 +1,132 @@
+import numpy as np
+import random
+from pathlib import Path
+from dss import DSS as dss_engine
+
+def pegar_fase():
+    DSSCircuit.SetActiveElement(DSSMonitors.Element)
+    DSSCircuit.SetActiveBus(DSSCircuit.ActiveCktElement.BusNames[0])
+    fase = list(DSSCircuit.Buses.VMagAngle).index(DSSMonitors.Channel(1)) / 2
+
+    return fase
+
+
+def InitializeDSS() -> tuple:
+    DSSObj = dss_engine
+    flag = DSSObj.Start(0)
+    if flag:
+        print('OpenDSS COM Interface initialized succesfully.')
+        
+    else:
+        print('OpenDSS COMInterface failed to start.')
+        
+    #Set up the interface variables - Comunication OpenDSS with Python
+    DSSText = DSSObj.Text
+    DSSCircuit = DSSObj.ActiveCircuit
+    DSSMonitors = DSSCircuit.Monitors
+            
+    return DSSCircuit, DSSText, DSSObj, DSSMonitors
+
+def desvio_padrao(vet_med: np.array) -> np.array:
+    pr = 0.002
+    dp = (pr * vet_med)/(3*1000)
+    return dp
+
+def iniciar_medidores(num_med_pot: int, num_med_tensao: int) -> None:
+    #Define aleatóriamente as linhas para medir o fluxo de potência
+    elementos = random.sample(DSSCircuit.Lines.AllNames, num_med_pot)
+    for i, elem in enumerate(elementos):
+        DSSText.Command = f'New Monitor.{i} element=Line.{elem}, terminal=1, mode=1'
+        
+    #Define aleatóriamente os elementos para medir as tensões na barra em que eles estão conectados
+    elementos = random.sample(DSSCircuit.AllElementNames, num_med_tensao)
+    for i, elem in enumerate(elementos):
+        DSSText.Command = f'New Monitor.{num_med_pot + i} element={elem}, terminal=1, mode=32'
+    
+def medidas(num_med_pot: int) -> np.array:
+    DSSMonitors.SampleAll()
+    DSSMonitors.SaveAll()
+    
+    vet_med = np.zeros(DSSMonitors.Count)
+    DSSMonitors.First
+    for i in range(DSSMonitors.Count):
+        medida = DSSMonitors.Channel(1)
+        if i >= num_med_pot:
+            DSSCircuit.SetActiveElement(DSSMonitors.Element)
+            DSSCircuit.SetActiveBus(DSSCircuit.ActiveCktElement.BusNames[0])
+            medida = medida / (DSSCircuit.Buses.kVBase*1000)
+        vet_med[i] = medida
+        DSSMonitors.Next
+    
+    return vet_med
+   
+def Calcula_residuo(vet_med: np.array, vet_estados: np.array, num_med_pot: int, num_med_tensao: int) -> np.array:
+    vetor_residuos = []
+    
+    DSSMonitors.First
+    for i in range(DSSMonitors.Count):
+        if DSSMonitors.Mode == 32:
+            tensao = vet_med[i]
+            #Acha a fase da medida, 0 é a fase 'a', 1 é a fase 'b' e 2 é a fase 'c'
+            fase = pegar_fase()
+            tensao_estimada = 0
+            vetor_residuos.append(tensao - tensao_estimada)
+        DSSMonitors.Next
+            
+def Calcula_Jacobiana(vet_med: np.array, vet_estados: np.array, num_med_pot: int, num_med_tensao: int) -> np.array:
+    num_medidas = num_med_tensao + num_med_pot
+    jacobiana = np.zeros((num_medidas, len(vet_estados)))
+    
+    DSSMonitors.First
+    for _ in range(DSSMonitors.Count):
+        if DSSMonitors.Mode == 32:
+            DSSCircuit.SetActiveElement(DSSMonitors.Element)
+            DSSCircuit.SetActiveBus(DSSCircuit.ActiveCktElement.BusNames[0])
+            continue
+
+        elif DSSMonitors.Mode == 1:
+            DSSCircuit.SetActiveElement(DSSMonitors.Element)
+            DSSCircuit.SetActiveBus(DSSCircuit.ActiveCktElement.BusNames[0])
+            
+            tensao1 = DSSCircuit.ActiveBus.puVoltages[0]
+            
+        DSSMonitors.Next
+        
+    return jacobiana
+
+#Achar o path do script do OpenDSS
+path = Path(__file__)
+CurrentFolder = path.parent
+MasterFile = CurrentFolder / '13Bus' / 'IEEE13Nodeckt.dss'
+
+#Inicializar variaveis
+num_med_tensao = 5
+num_med_pot = 5
+num_medidas = num_med_pot + num_med_tensao
+
+DSSCircuit, DSSText, DSSObj, DSSMonitors = InitializeDSS()
+DSSObj = dss_engine
+DSSText = DSSObj.Text
+DSSCircuit = DSSObj.ActiveCircuit
+DSSMonitors = DSSCircuit.Monitors
+
+DSSText.Command = f'Compile {MasterFile}'
+
+iniciar_medidores(num_med_pot, num_med_tensao)
+
+DSSText.Command = 'Solve'
+
+vet_med = medidas(num_med_pot)
+print(vet_med)
+
+#Inicializar o vetor de estados com perfil de tensão neutro
+vet_estados = np.zeros(DSSCircuit.NumBuses*6)
+for i in range(DSSCircuit.NumBuses*3):
+    vet_estados[i] = 1
+
+#des_pad = desvio_padrao((vet_med * 0.002)/300)
+
+jacobiana = Calcula_Jacobiana(vet_med, vet_estados, num_med_pot, num_med_tensao)
+
+residuos = Calcula_residuo(vet_med, vet_estados, num_med_pot, num_med_tensao)
+
---
+++ ex12_6_monticelli.py
@@ -0,0 +1,95 @@
+import numpy as np
+
+def Calcula_Jacobiana(vet_estados: np.array, num_med: int) -> np.array:
+    H = np.zeros((num_med, len(vet_estados)))
+    mag1 = vet_estados[1]
+    mag2 = vet_estados[2]
+    magt = mag1*mag2
+    anglediff = -vet_estados[0]
+    H[0, 0] = -magt*g*np.sin(anglediff)+magt*b*np.cos(anglediff)
+    H[0, 1] = 2*mag1*g-mag2*g*np.cos(anglediff)-mag2*b*np.sin(anglediff)
+    H[0, 2] = -mag1*g*np.cos(anglediff)-mag1*b*np.sin(anglediff)
+    H[1, 0] = magt*g*np.sin(-anglediff)-magt*b*np.cos(-anglediff)
+    H[1, 1] = -mag2*g*np.cos(-anglediff)-mag2*b*np.sin(-anglediff)
+    H[1, 2] = 2*mag2*g-mag1*g*np.cos(-anglediff)-mag1*b*np.sin(-anglediff)
+    H[2, 0] = magt*b*np.sin(anglediff)+magt*g*np.cos(anglediff)
+    H[2, 1] = -2*mag1*(b+bsh)+mag2*b*np.cos(anglediff)-mag2*g*np.sin(anglediff)
+    H[2, 2] = mag1*b*np.cos(anglediff)-mag1*g*np.sin(anglediff)
+    H[3, 0] = -magt*b*np.sin(-anglediff)-magt*g*np.cos(-anglediff)
+    H[3, 1] = mag2*b*np.cos(-anglediff)-mag2*g*np.sin(-anglediff)
+    H[3, 2] = -2*mag2*(b+bsh)+mag1*b*np.cos(-anglediff)-mag1*g*np.sin(-anglediff)
+    H[4, 0] = 0
+    H[4, 1] = 1
+    H[4, 2] = 0
+    return H
+
+def Calcula_Residuo(vet_med: np.array, vet_estados: np.array) -> np.array:
+    mag1 = vet_estados[1]
+    mag2 = vet_estados[2]
+    magt = mag1*mag2
+    anglediff = -vet_estados[0]
+
+    residuos = []
+    #Calcula as estimativas das potências ativas
+    for medida, mag in zip(vet_med[:2], (mag1, mag2)):
+        estimativa = (mag**2)*g-magt*g*np.cos(anglediff)-magt*b*np.sin(anglediff)
+        residuos.append(medida - estimativa)
+        anglediff = -anglediff
+        
+    #Calcula as estimativas das potências reativas
+    for medida, mag in zip(vet_med[2:4], (mag1, mag2)):
+        estimativa = -(mag**2)*(b+bsh)+magt*b*np.cos(anglediff)-magt*g*np.sin(anglediff)
+        residuos.append(medida - estimativa)
+        anglediff = -anglediff
+        
+    #Calcula a estimativa da Tensão
+    residuos.append(vet_med[4] - vet_estados[1])
+        
+    return residuos
+
+def Calcula_Pesos(num_med: int, des_pad: np.array) -> np.array:
+    matriz_pesos = np.zeros((num_med, num_med))
+
+    for i, dp in enumerate(des_pad):
+        matriz_pesos[i, i] = 1/(dp**2)
+        
+    return matriz_pesos
+
+def EE(vet_med: np.array, vet_estados: np.array, matriz_pesos: np.array, erro_max: float) -> np.array:
+    delx = 1
+    while(np.max(delx) > erro_max):
+        jacobiana = Calcula_Jacobiana(vet_estados, len(vet_med))
+        
+        residuo = np.array(Calcula_Residuo(vet_med, vet_estados))
+
+        #Calcula a matriz ganho
+        matriz_ganho = np.dot(np.dot(jacobiana.T, matriz_pesos), jacobiana)
+
+        #Calcula o outro lado da Equação normal
+        seinao = np.dot(np.dot(jacobiana.T, matriz_pesos), residuo)
+
+        delx = np.dot(np.linalg.inv(matriz_ganho), seinao)
+        
+        #Atualiza o vetor de estados
+        vet_estados[0] += delx[0]
+        vet_estados[1:] += delx[1:]
+    
+    return vet_estados
+
+#Inicializa os valores de modelagem da linha dados na questão
+y = 1 / (0.0062 + 0.036j)
+g = np.real(y)
+b = np.imag(y)
+bsh = 0.0104/2
+
+#Inicializa as medidas dadas na questão
+vet_med = np.array([1.711, -1.614, 0.2245, -0.1566, 1.016])
+
+vet_estados = np.array([0, 1, 1], dtype=float)
+
+des_pad = np.array([1 for _ in range(5)])
+
+W = Calcula_Pesos(len(vet_med), des_pad)
+
+vet_estados = EE(vet_med, vet_estados, W, 10**-3)
+print(vet_estados)
